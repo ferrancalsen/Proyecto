9645fc96c154c441f39066cef5c9b54db8561d3d
ferrancalsen <fcalonges@gmail.com>
2024-05-10 18:51:46 +0200

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-admin:9.2.0")
 }
